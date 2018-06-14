75277e00457be5a971cdbb0dbd3d64f86f9fd4a1
zabuzooo <corocoo.pass@gmail.com>
2018-06-14 11:24:57 +0000

    view変更,ヘッダーフッダー追加,review機能まだ途中

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ApplicationRecord
 	belongs_to :label, optional: true
 	belongs_to :genre, optional: true
 	has_many :discs, dependent: :destroy
+	has_many :reviews, dependent: :destroy
 end

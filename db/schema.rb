94d8f256970b4c93f10d028f76c1b2b20474fc68
fumu238 <fumu238@gmail.com>
2018-06-24 07:30:04 +0000

    エラー,コンタクトなど

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180610102501) do
+ActiveRecord::Schema.define(version: 20180624070901) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -47,6 +47,14 @@ ActiveRecord::Schema.define(version: 20180610102501) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "type"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

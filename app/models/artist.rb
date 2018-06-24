29312c564d6d5c35fd7504892256951fe7fb99dd
wananab1 <wtnbshut.mij@gmail.com>
2018-06-24 06:33:10 +0000

    user検索

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,5 @@
 class Artist < ApplicationRecord
 	has_many :products, dependent: :destroy
 	has_many :tunes, dependent: :destroy
+	validates :artist_name, :artist_phonetic, presence: true
 end
--- app/models/label.rb
+++ app/models/label.rb
@@ -1,3 +1,4 @@
 class Label < ApplicationRecord
 	has_many :product, dependent: :destroy
+	validates :label_name, presence: true
 end

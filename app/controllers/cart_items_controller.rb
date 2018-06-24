bdbaaace06b4564848c14b87ea78c0aa906ec143
fumu238 <fumu238@gmail.com>
2018-06-24 08:35:01 +0000

    order/cartitemコントローラなど

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -3,22 +3,36 @@ class CartItemsController < ApplicationController
    def create
 		product = Product.find_by(id:  params[:product_id])
 		cart = Cart.find_by(user_id: current_user.id)
-
+		
 		if cart_item = CartItem.find_by(product_id: product.id, cart_id: cart.id)
 			i = params[:cart_item][:cart_quantity]
+			unless i.blank?
 			cart_item.cart_quantity += i.to_i
-			cart_item.save
+		    cart_item.save
+			redirect_to product_path(product)
+			 flash[:complete] = "カートに商品が入りました"
+			 return
+		    else
+		    redirect_to product_path(product)
+		     flash[:complete] = "もっと買ってください"
+		     return
+		    end
 		else
 			new_cart_item = CartItem.new(cart_item_params)
 			new_cart_item.product_id = product.id
 			new_cart_item.cart_id = cart.id
-			new_cart_item.save
-		 end
-
-	    redirect_to product_path(product)
-
+			if new_cart_item.save
+			 redirect_to product_path(product)
+			 flash[:complete] = "カートに商品が入りました"
+			return
+		    else
+		     redirect_to product_path(product)
+		     flash[:complete] = "もっと買ってください"
+		    return
+		    end
+		end
 	end
-
+	
 	def destroy
 		cart_item = CartItem.find(params[:id])
 		cart = Cart.find_by(user_id: current_user.id)
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,5 +1,7 @@
 class CartItem < ApplicationRecord
 
+  validates :cart_quantity, presence:true
+
   belongs_to :product
   belongs_to :cart
 
